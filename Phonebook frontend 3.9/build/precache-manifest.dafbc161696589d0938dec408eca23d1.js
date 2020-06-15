b3986d00a0823c1112db4cf452b2ce3bcf8c7f93
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-06-15 18:00:54 +0300

    feat: generate build

---
+++ Phonebook frontend 3.9/build/precache-manifest.dafbc161696589d0938dec408eca23d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0024d2a12f73fed4fb043e49cfb7964e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a850667409e79b6dbc8",
+    "url": "/static/css/main.e718c16a.chunk.css"
+  },
+  {
+    "revision": "be80ab81545a948c8dfe",
+    "url": "/static/js/2.ed0afba3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ed0afba3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a850667409e79b6dbc8",
+    "url": "/static/js/main.2eb589a9.chunk.js"
+  },
+  {
+    "revision": "0ef065ed55894c1367de",
+    "url": "/static/js/runtime-main.2728da5d.js"
+  }
+]);
