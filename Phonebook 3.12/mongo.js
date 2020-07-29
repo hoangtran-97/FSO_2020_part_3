ec90b268cd56e38730fb21f939c64e04f020f2a5
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-07-29 10:08:28 +0300

    feat: 3.12

---
+++ Phonebook 3.12/mongo.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+    console.log("Please provide the password as an argument: node mongo.js <password>");
+    process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+const url = `mongodb+srv://test_user:${password}@cluster0.knwkw.mongodb.net/phonebook-app?retryWrites=true&w=majority`;
+
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+    name,
+    number,
+});
+
+person.save().then((result) => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+});
