bc439b077fe82bfbf87b3128530b824a1b50230f
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-08-04 11:23:48 +0300

    feat: deleting

--- Phonebook 3.12/index.js
+++ Phonebook 3.12/index.js
@@ -65,11 +65,12 @@ app.get("/api/persons/:id", (request, response) => {
     });
 });
 //Delete
-app.delete("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id);
-    persons = persons.filter((person) => person.id !== id);
-
-    response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then((result) => {
+            response.status(204).end();
+        })
+        .catch((error) => next(error));
 });
 //Post Mongo
 app.post("/api/persons", (request, response) => {
@@ -111,6 +112,17 @@ app.post("/api/persons", (request, response) => {
         response.json(savedPerson);
     });
 });
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === "CastError") {
+        return response.status(400).send({error: "malformatted id"});
+    }
+
+    next(error);
+};
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
