356990db8d26966c865ef310edc335af3420fba9
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-08-04 11:57:19 +0300

    feat: update

--- Phonebook 3.12/index.js
+++ Phonebook 3.12/index.js
@@ -112,6 +112,20 @@ app.post("/api/persons", (request, response) => {
         response.json(savedPerson);
     });
 });
+//Put Mongo
+app.put("/api/persons/:id", (request, response, next) => {
+    const body = request.body;
+
+    const person = {
+        number: body.number,
+    };
+
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+        .then((updatedPerson) => {
+            response.json(updatedPerson);
+        })
+        .catch((error) => next(error));
+});
 const errorHandler = (error, request, response, next) => {
     console.error(error.message);
 
